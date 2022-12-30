c79d95541b7be68af5855b74707379965af90002
Giulivan Sousa <giulivan_cs@ghotmail.com>
2022-12-30 11:44:56 -0300

    Refatorando secoes comuns de video em componentes

--- resources/views/client-info/index.blade.php
+++ resources/views/client-info/index.blade.php
@@ -70,77 +70,17 @@
             </button>
         </section>
         <div class="division"></div>
-        <section class="section">
-            <h2 class="section-title">O que faremos:</h2>
-            <ul class="section-list">
-                @foreach ($videosWhatDo as $video)
-                    <li class="section-videos__item card">
-                        <div class="list-videobox">
-                            <iframe class="video video-list" width="560" height="315" src="https://www.youtube.com/embed/{{ $video->url }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                        <h4 class="section-videos__item-title">{{ $video->title }}</h4>
-                        <p class="section-videos__item-description">{{ $video->description }}</p>
-                        <div class="menu__edit-delete list-btn">
-                            <a href="/editar-video" class="btn-delete--content card">Editar</a>
-                            <a href="/excluir-video" class="btn-delete--content card">Excluir</a>
-                        </div>
-                    </li>
-                @endforeach
-                <button class="item-btn-add">
-                    <a href="{{ route('video.create', ['id' => $id, 'tag' => 'whatDo']) }}" class="btn-add-border">
-                        <span>Adicionar novo conteúdo</span>
-                    </a>
-                </button>
-            </ul>
-        </section>
+
+        <x-video.card :title="'O que faremos'" :videos="$videosWhatDo" :id="$id" :tag="'whatDo'" />
+
         <div class="division"></div>
-        <section class="section">
-            <h2 class="section-title">Como faremos:</h2>
-            <ul class="section-list">
-                @foreach ($videosHowDo as $video)
-                    <li class="section-videos__item card">
-                        <div class="list-videobox">
-                            <iframe class="video video-list" width="560" height="315" src="https://www.youtube.com/embed/{{ $video->url }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                        <h4 class="section-videos__item-title">{{ $video->title }}</h4>
-                        <p class="section-videos__item-description">{{ $video->description }}</p>
-                        <div class="menu__edit-delete list-btn">
-                            <a href="/editar-video" class="btn-delete--content card">Editar</a>
-                            <a href="/excluir-video" class="btn-delete--content card">Excluir</a>
-                        </div>
-                    </li>
-                @endforeach
-                <button class="item-btn-add">
-                    <a href="{{ route('video.create', ['id' => $id, 'tag' => 'howDo']) }}" class="btn-add-border">
-                        <span>Adicionar novo conteúdo</span>
-                    </a>
-                </button>
-            </ul>
-        </section>
+        
+        <x-video.card :title="'Como faremos'" :videos="$videosHowDo" :id="$id" :tag="'howDo'" />
+        
         <div class="division"></div>
-        <section class="section">
-            <h2 class="section-title">Processos da equipe:</h2>
-            <ul class="section-list">
-                @foreach ($videosTeam as $video)
-                    <li class="section-videos__item card">
-                        <div class="list-videobox">
-                            <iframe class="video video-list" width="560" height="315" src="https://www.youtube.com/embed/{{ $video->url }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                        <h4 class="section-videos__item-title">{{ $video->title }}</h4>
-                        <p class="section-videos__item-description">{{ $video->description }}</p>
-                        <div class="menu__edit-delete list-btn">
-                            <a href="/editar-video" class="btn-delete--content card">Editar</a>
-                            <a href="/excluir-video" class="btn-delete--content card">Excluir</a>
-                        </div>
-                    </li>
-                @endforeach
-                <button class="item-btn-add">
-                    <a href="{{ route('video.create', ['id' => $id, 'tag' => 'team']) }}" class="btn-add-border">
-                        <span>Adicionar novo conteúdo</span>
-                    </a>
-                </button>
-            </ul>
-        </section>
+
+        <x-video.card :title="'Processos da equipe'" :videos="$videosTeam" :id="$id" :tag="'team'" />
+
     <x-footer />
     </main> 
 </x-layout>
---
+++ resources/views/components/video/card.blade.php
@@ -0,0 +1,23 @@
+<section class="section">
+    <h2 class="section-title">{{ $title }}</h2>
+    <ul class="section-list">
+        @foreach ($videos as $video)
+            <li class="section-videos__item card">
+                <div class="list-videobox">
+                    <iframe class="video video-list" width="560" height="315" src="https://www.youtube.com/embed/{{ $video->url }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+                <h4 class="section-videos__item-title">{{ $video->title }}</h4>
+                <p class="section-videos__item-description">{{ $video->description }}</p>
+                <div class="menu__edit-delete list-btn">
+                    <a href="/editar-video" class="btn-delete--content card">Editar</a>
+                    <a href="/excluir-video" class="btn-delete--content card">Excluir</a>
+                </div>
+            </li>
+        @endforeach
+        <button class="item-btn-add">
+            <a href="{{ route('video.create', ['id' => $id, 'tag' => $tag]) }}" class="btn-add-border">
+                <span>Adicionar novo conteúdo</span>
+            </a>
+        </button>
+    </ul>
+</section>
